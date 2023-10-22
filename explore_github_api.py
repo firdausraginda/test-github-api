3292a1d300130aee4e0011e36ad472e9b88233b7
firdausraginda <ragindafirdaus01@gmail.com>
2023-10-22 17:40:27 +0700

    create add_commit()

--- explore_github_api.py
+++ explore_github_api.py
@@ -271,6 +271,38 @@ class GithubFunctionality:
             output["data"] = response.json()
 
         return output
+
+    def add_commit(self, repo_name: str) -> dict:
+        "add commit"
+
+        output = {
+            "status": True,
+            "data": {},
+            "error": []
+        }
+
+        try:
+            # data = '{"message":"my commit message","author":{"name":"Mona Octocat","email":"octocat@github.com","date":"2008-07-09T16:13:30+12:00"},"parents":["7d1b31e74ee336d15cbd21741bc88a537ed063a0"],"tree":"827efc6d56897b048c772eb4087f854f46256132","signature":"-----BEGIN PGP SIGNATURE-----\\n\\niQIzBAABAQAdFiEESn/54jMNIrGSE6Tp6cQjvhfv7nAFAlnT71cACgkQ6cQjvhfv\\n7nCWwA//XVqBKWO0zF+bZl6pggvky3Oc2j1pNFuRWZ29LXpNuD5WUGXGG209B0hI\\nDkmcGk19ZKUTnEUJV2Xd0R7AW01S/YSub7OYcgBkI7qUE13FVHN5ln1KvH2all2n\\n2+JCV1HcJLEoTjqIFZSSu/sMdhkLQ9/NsmMAzpf/iIM0nQOyU4YRex9eD1bYj6nA\\nOQPIDdAuaTQj1gFPHYLzM4zJnCqGdRlg0sOM/zC5apBNzIwlgREatOYQSCfCKV7k\\nnrU34X8b9BzQaUx48Qa+Dmfn5KQ8dl27RNeWAqlkuWyv3pUauH9UeYW+KyuJeMkU\\n+NyHgAsWFaCFl23kCHThbLStMZOYEnGagrd0hnm1TPS4GJkV4wfYMwnI4KuSlHKB\\njHl3Js9vNzEUQipQJbgCgTiWvRJoK3ENwBTMVkKHaqT4x9U4Jk/XZB6Q8MA09ezJ\\n3QgiTjTAGcum9E9QiJqMYdWQPWkaBIRRz5cET6HPB48YNXAAUsfmuYsGrnVLYbG+\\nUpC6I97VybYHTy2O9XSGoaLeMI9CsFn38ycAxxbWagk5mhclNTP5mezIq6wKSwmr\\nX11FW3n1J23fWZn5HJMBsRnUCgzqzX3871IqLYHqRJ/bpZ4h20RhTyPj5c/z7QXp\\neSakNQMfbbMcljkha+ZMuVQX1K9aRlVqbmv3ZMWh+OijLYVU2bc=\\n=5Io4\\n-----END PGP SIGNATURE-----\\n"}'
+            payload = json.dumps({
+                "tree":"6711e52f55719b012fd825156462e8c6e9bbc042",
+                "message":"add helloworld/main.py",
+                "parents": ["9530a16ee4445775be609b8ed855f6abb719d08c"]
+            })
+
+            response = requests.post(
+                url=f"{URL_GITHUB_API}/{repo_name}/git/commits",
+                headers=self.__headers,
+                data=payload
+            )
+
+        except Exception as e:
+            output["status"] = False
+            output["error"].append(f"[github_functionality - add_commit] {e}")
+
+        else:
+            output["data"] = response.json()
+
+        return output
     
     
 if __name__ == "__main__":
@@ -326,4 +358,9 @@ if __name__ == "__main__":
     # ------------------------------------------------------------------
 
     # res = github_func_obj.post_tree(REPOSITORY_NAME)
+    # print(json.dumps(res))
+
+    # ------------------------------------------------------------------
+
+    # res = github_func_obj.add_commit(REPOSITORY_NAME)
     # print(json.dumps(res))
